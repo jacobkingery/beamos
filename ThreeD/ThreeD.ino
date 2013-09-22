c3737bb3afbdcc8940702ffe71e4e55a8dd61271
Jacob Kingery <jacob.kingery@students.olin.edu>
2013-09-22 01:23:07 -0400

    moved distance fitting to arduino, eliminated live plotting

--- ThreeD/ThreeD.ino
+++ ThreeD/ThreeD.ino
@@ -1,3 +1,8 @@
+/*
+Principles of Engineering, Fall 2013
+Lab 1:  DIY LIDAR
+Jacob Kingery and Myles Cooper
+*/
 
 #include <Servo.h> 
  
@@ -5,47 +10,52 @@ Servo panServo;  //create servo object for the pan servo
 Servo tiltServo;  //create servo object for the tilt servo
 
 
-const int panPin = 9; 
-const int tiltPin = 10;
-const int sensorPin = A0;
+const int panPin = 9;  //pan servo pin
+const int tiltPin = 10;  //tilt servo pin
+const int sensorPin = A0;  //IR range sensor pin
 
 int panPos = 5;  //pan position 
-int tiltPos = 45;  //tilt position
-int totDist;
-float aveDist;
+int tiltPos = 50;  //tilt position
+int totDist;  //total distance of 5 shots
+float aveDist;  //average distance of 5 shots
+float realDist;  //voltage converted to real distance
  
 void setup() { 
   Serial.begin(9600);  //initialize serial communication
 
   panServo.attach(panPin);  //attach the pan servo
   tiltServo.attach(tiltPin);  //attach the tilt servo
+  
   panServo.write(panPos);  //turn pan servo to 5 degrees
   tiltServo.write(tiltPos);  //turn tilt servo to 45 degrees
   
-  establishConnection();
+  establishConnection();  //establish connection with python script
 } 
  
 void establishConnection() {
-  delay(500);  //wait to ensure that serial connection is made
   Serial.println("Arduino ready!");  //send ready signal to python script
   while (!Serial.available()) {  //wait for python script to send ready signal
   }
   while (Serial.available()) {
     Serial.read();
   }  
-  delay(500);
+  delay(500);  //wait a bit before starting to stream data
 }
 
 
 void shoot() {
   totDist = 0;
-  delay(50);
+  delay(250);
   for (int i=0; i<5; i++) {
     totDist += analogRead(sensorPin);
     delay(50);
   }
   aveDist = totDist / 5.0;
-  Serial.print(aveDist);
+  
+  float w = (aveDist - 280)/120;
+  realDist = .62*pow(w, 4) - 2.4*pow(w, 3) + 3.4*w*w - 7.3*w + 18;
+  
+  Serial.print(realDist);
   Serial.print('@');
   Serial.print(panPos);
   Serial.print('@');  
@@ -53,32 +63,32 @@ void shoot() {
 } 
  
 void loop() {
-  for (panPos=5; panPos<175; panPos++) {
+  for (panPos=5; panPos<175; panPos+=5) {
     panServo.write(panPos);
     if (panPos%2 != 0) {
-      for (tiltPos=45; tiltPos<=100; tiltPos++) {
+      for (tiltPos=50; tiltPos<=100; tiltPos+=10) {
         tiltServo.write(tiltPos);
         shoot();
       }
     }
     else {
-      for (tiltPos=100; tiltPos>=45; tiltPos--) {
+      for (tiltPos=100; tiltPos>=50; tiltPos-=10) {
         tiltServo.write(tiltPos);
         shoot();
       }
     }
   }
   
-  for (panPos=175; panPos>5; panPos--) {
+  for (panPos=175; panPos>5; panPos-=5) {
     panServo.write(panPos);
     if (panPos%2 != 0) {
-      for (tiltPos=45; tiltPos<=100; tiltPos++) {
+      for (tiltPos=50; tiltPos<=100; tiltPos+=10) {
         tiltServo.write(tiltPos);
         shoot();
       }
     }
     else {
-      for (tiltPos=100; tiltPos>=45; tiltPos--) {
+      for (tiltPos=100; tiltPos>=50; tiltPos-=10) {
         tiltServo.write(tiltPos);
         shoot();
       }
--- pantilt.py
+++ pantilt.py
@@ -1,3 +1,9 @@
+'''
+Principles of Engineering, Fall 2013
+Lab 1:  DIY LIDAR
+Jacob Kingery and Myles Cooper
+'''
+
 import serial
 import time
 import math
@@ -7,8 +13,8 @@ from mpl_toolkits.mplot3d import Axes3D
 def receive():
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	plt.ion()
-	plt.show()
+	# plt.ion()
+	# plt.show()
 	plt.title('3D View')
 	ax.set_xlabel('X (cm)')
 	ax.set_ylabel('Y (cm)')
@@ -17,50 +23,61 @@ def receive():
 	ax.set_xlim(-100, 100)
 	ax.set_ylim(-10, 100)
 	ax.set_zlim(-50, 50)
-	ax.scatter(0, 0, 0, s=20, c='b')
-	plt.draw()
+	ax.scatter(0, 0, 0, s=30, c='b')
+	# plt.draw()
 	colors = ['r', 'g', 'm', 'y', 'c']
 	clr = 0
 	sweep = 0
 	trigger = 0
-	while 1:
+	x = []
+	y = []
+	z = []
 
+	for i in range(35*6):
 		raw_data = ser.readline().strip().split('@')
 
-		raw_dist = float(raw_data[0])
-		w = (raw_dist - 280.0)/120.0
-		dist = .62*w**4 - 2.4*w**3 + 3.4*w**2 - 7.3*w + 18
+		# raw_dist = float(raw_data[0])
+		# w = (raw_dist - 280.0)/120.0
+		# dist = .62*w**4 - 2.4*w**3 + 3.4*w**2 - 7.3*w + 18
+		dist = float(raw_data[0])
 
 		theta = -math.radians(int(raw_data[1]) - 90)  #convert degrees to radians and shift so that the range is -90 to 90 degrees
 		phi = math.radians(int(raw_data[2]) - 90)
 
 		xyDist = math.cos(phi) * dist
 
-		z = math.sin(phi) * dist * 2.54
-		x = math.sin(theta) * xyDist * 2.54  #convert from polar coordinates to
-		y = math.cos(theta) * xyDist * 2.54  #cartesian coodinates and inches to mm
+		z.append(math.sin(phi) * dist * 2.54)
+		x.append(math.sin(theta) * xyDist * 2.54)  #convert from polar coordinates to
+		y.append(math.cos(theta) * xyDist * 2.54)  #cartesian coodinates and inches to mm
 
+		# if  abs(theta) == math.radians(85):  #cycle the color and save the figure each sweep
+		# 	trigger += 1
+		# 	if trigger == 1:
+		# 		clr = (clr + 1)*(clr != len(colors) - 1)
+		# 		plt.savefig('./Results/sweep{0}.png'.format(sweep)) 
+		# 		sweep += 1
+		# if abs(theta) != math.radians(85):
+		# 	trigger = 0
+	print('Done receiving')
+	ser.close()
+	ser2 = serial.Serial('/dev/ttyACM0', 9600)
+	ax.scatter(x, y, z, s=20, c=colors[clr], marker='o')  #plot the point
+	plt.show()  #update plot
+	print('Done plotting')
 
-		if  abs(theta) == math.radians(85):  #cycle the color each sweep
-			trigger += 1
-			if trigger == 1:
-				clr = (clr + 1)*(clr != len(colors) - 1)
-				plt.savefig('./Results/test{0}.png'.format(sweep)) 
-				sweep += 1
-		if  abs(theta) == math.radians(45):
-			trigger = 0
-
-		ax.scatter(x, y, z, s=20, c='g', marker='.')  #plot the point
-		plt.draw()  #update plot
+def establishContact():
+	time.sleep(.5)
+	ser.flushInput()
+	time.sleep(1)
+	print(ser.readline().strip())
+	print('Python ready!')
+	ser.write("Python ready!")	
 
 
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyACM0', 9600)
-	time.sleep(1)
-	ser.flushInput()
-	time.sleep(2)
-	print(ser.readline().strip())
-	print('Python ready!')
-	ser.write("Python ready!")
+	establishContact()
+	print('Contact established, receiving data...')
 	receive()
-	ser.close()
+	time.sleep(1)
+
